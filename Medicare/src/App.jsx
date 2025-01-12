46fc89e7962bfd285e277d4ea216ca980a956714
Carlstorm <carlglainnyuysemo@gmail.com>
2025-01-12 19:38:28 +0100

    initial commit to deploy on vessel

---
+++ Medicare/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to medifinder </h1>
+      <p>Find hospitals and pharmacies around you easily</p>
+    </div>
+  )
+}
+
+export default App
