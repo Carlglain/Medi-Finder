aa94b0d8e24d80381594b7350e65e4aa5d90f52e
Carlstorm <carlglainnyuysemo@gmail.com>
2025-01-14 20:22:20 +0100

    registration page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import SignUp from './Pages/SignUp';
 function App() {
   return (
     <div>
-      <h1>Welcome to medifinder </h1>
-      <p>Find hospitals and pharmacies around you easily</p>
+      <SignUp />
+      {/* <h1>Welcome to medifinder </h1>
+      <p>Find hospitals and pharmacies around you easily</p> */}
     </div>
   )
 }
---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./registration.css"
+function SignUp() {
+  return (
+    <div className='signup-container'>
+      <form className='signup-form'>
+        <label htmlFor="l1">First Name</label>
+        <input id='l1' type="text" placeholder='john' name='FName'/>
+        <label htmlFor="l2">Last Name</label>
+        <input id='l2' type="text" placeholder='doe' name='LName'/>
+        <br />
+        <label htmlFor="l3">Phone Number</label>
+        <input id='l3' type="number" name='phone' placeholder='670483932'/>
+        <br />
+        <label htmlFor="l4">Email Address</label>
+        <input id='l4' type="text" placeholder='example@gmail.com' />
+        <br />
+        <label htmlFor="l5">Location/Address</label>
+        <input id='l5' type="text" placeholder='molyko' />
+        <button>Sign Up</button>
+        <p>Already have an Account?</p>
+        <button>Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
