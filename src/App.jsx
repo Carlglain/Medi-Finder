e1db7b202de56fad219bfb6510254be208aa17c0
Carlstorm <carlglainnyuysemo@gmail.com>
2025-02-25 22:10:25 +0100

    designing my account page

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
   return (
     <div>
       <NavBar />
-    
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/pharmacy' element={<PharmacieList />} />
--- src/Pages/Account/Myaccount.jsx
+++ src/Pages/Account/Myaccount.jsx
@@ -1,28 +1,62 @@
-import React from 'react'
-import './myaccount.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./myaccount.css";
+import { Link } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
 function Myaccount() {
   return (
-    <div className='account-container'>
-      {/* login
-      logout
-      userinfo
+    <div className="account-container">
+      {/*
       favourites
       last visited */}
       {/* if user is logged in */}
-      
-      <h1>User Profile </h1>
-     
-      <p><span>Full Name</span> <span> name here</span> </p>
-      <p><span>Email</span> <span> email here</span> </p>
-      <p><span>Phone</span> <span> Phone number here</span> </p>
-      <p><span>Address</span> <span> Address here</span> </p>
-      
-      {/* if user is not logged in */}
-      <div><Link to='/login'>Login to see your info</Link></div> 
-  
+      <div className="user-profile">
+        <h1 className="blur-header">Current User </h1>
+        <div>
+          <AccountCircleIcon   /> 
+          <span>User Name</span>
+          <span className="user-name"> name here</span>
+        </div>
+        <hr />
+        <div className="account-content">
+          <div className="more-info">
+        <h2 className="blur-header">More info</h2>
+        <p>
+          <span>Email</span> <span> email here</span>{" "}
+        </p>
+        <p>
+          <span>Phone</span> <span> Phone number here</span>{" "}
+        </p>
+        <p>
+          <span>Address</span> <span> Address here</span>{" "}
+        </p>
+        <button>Edit Profile</button>
+          <button>Logout</button>
+          </div>
+        {/* if user is not logged in */}
+        <div>
+          <Link to="/login">Login Options</Link>
+        </div>
+    
+      <div className="favourites">
+        <h1>Your favourite pharmacy</h1>
+        <p>data shows your'e a fan of </p>
+        {/* <p>{fav-pharmacy}</p> */}
+      </div>
+      <div className="favourites">
+        <h1>Recents</h1>
+        <p>Enamen pharmacy - 10/3/2024 </p>
+      {/* follow the format on the comments to do the correct thing */}
+        {/* {
+          recents.map((index,recent) =>(
+        <p>{recent.pharmacy} pharmacy - {recent.date} </p>
+        ))
+        } */}
+      </div>
+      </div>
     </div>
-  )
+    </div>
+  );
 }
 
-export default Myaccount
+export default Myaccount;
